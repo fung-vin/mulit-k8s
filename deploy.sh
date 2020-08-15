452b71d25e4b0455ef67332d2dc4f2801c2f0cee
Vince <fung.vin@gmail.com>
2020-08-15 23:48:08 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t xdcc/multi-client:latest -t xdcc/multo-client:$SHA -f ./client/Dockerfile ./client
-docker build -t xdcc/multi-server:latest -t xdcc/multo-server:$SHA -f ./server/Dockerfile ./server
-docker build -t xdcc/multi-worker:latest -t xdcc/multo-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t xdcc/multi-client:latest -t xdcc/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t xdcc/multi-server:latest -t xdcc/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t xdcc/multi-worker:latest -t xdcc/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push xdcc/multi-client:latest
 docker push xdcc/multi-server:latest
@@ -11,6 +11,6 @@ docker push xdcc/multi-server:$SHA
 docker push xdcc/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=xdcc/multi-client:$SHA
 kubectl set image deployments/server-deployment server=xdcc/multi-server:$SHA
-kubectl set image deployments/server-deployment server=xdcc/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=xdcc/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=xdcc/multi-worker:$SHA
